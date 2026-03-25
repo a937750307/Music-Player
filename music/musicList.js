675546b372d671960e84b025dab96d4905bb6a51
YiHengZhou <68763588+a937750307@users.noreply.github.com>
2026-03-25 12:24:11 +0800

    Upload musicList.js

---
+++ music/musicList.js
@@ -0,0 +1,39 @@
+/*
+ * Browser	    MP3	    WAV  	OGG
+ * IE9+	        YES	    NO	    NO
+ * Chrome	    YES	    YES 	YES
+ * Firefox	    YES 	YES 	YES
+ * Safari	    YES	    YES 	NO
+ * Opera	    YES	    YES	    YES
+ * Edge         YES     YES     NO
+ * Wallpaper    NO      YES     YES
+ */
+var musicList = [
+	"Approaching Nirvana - You.mp3",
+	"Cymophane - Tassel.mp3",
+	"Daylight.mp3",
+	"Fade - Alan Walker.mp3",
+	"Illusionary Daytime - Shirfine.mp3",
+	"July - My Soul.mp3",
+	"Otokaze - 夏恋.mp3",
+	"Piano Echoes - 蒲公英的約定 (Piano Ver.).mp3",
+	"River Flows In You (你的心河) (钢琴版).mp3",
+	"Snigellin - Sakura Tears.mp3",
+	"The Way I Still Love You.mp3",
+	"The truth that you leave - Pianoboy高至豪.mp3",
+	"Time Machine - CMJ.mp3",
+	"Valentin - A Little Story.mp3",
+	"Windy Hill.mp3",
+	"Zeraphym 六翼使徒 - Lifeline.mp3",
+	"城南花已开 - 三亩地.mp3",
+	"夏の喚く - 邱有句.mp3",
+	"夜的钢琴曲(五) - 石进.mp3",
+	"所念皆星河（钢琴版）.mp3",
+	"羽肿 - Rain after Summer.mp3",
+	"诀别书 - 邓垚.mp3",
+	"谷雨 伴奏 - 桑葚上的猴子.mp3",
+	"逆时针向 - Asphyxia.mp3",
+	"阿南亮子 - Refrain.mp3"
+];
+
+
